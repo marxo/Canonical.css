982a88205e2d23bd4a5b20b6d994b800f71981c7
Марко Кажић <marko_kazic@live.com>
2015-07-09 01:20:22 +0200

    Fixes for vertical alignment in em and i elements

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Lightweight CSS normalization for modern browsers.",
-  version: "1.0.8",
+  version: "1.0.9",
   name: "marxo:canonical.css",
   git: "https://github.com/marxo/canonical.css.git",
 });
